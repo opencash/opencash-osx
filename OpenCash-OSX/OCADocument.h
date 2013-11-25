4168448a214f5a344e940f5b1f5a7250083e4ae1
Serge Gebhardt <sgebhar@sgebhar-mbp.local>
2013-11-25 11:46:06 -0200

    Initial Commit

---
+++ OpenCash-OSX/OCADocument.h
@@ -0,0 +1,13 @@
+//
+//  OCADocument.h
+//  OpenCash-OSX
+//
+//  Created by Serge Gebhardt on 11/25/13.
+//  Copyright (c) 2013 Serge Gebhardt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface OCADocument : NSDocument
+
+@end
---
+++ OpenCash-OSX/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  OpenCash-OSX
+//
+//  Created by Serge Gebhardt on 11/25/13.
+//  Copyright (c) 2013 Serge Gebhardt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+  return NSApplicationMain(argc, argv);
+}
